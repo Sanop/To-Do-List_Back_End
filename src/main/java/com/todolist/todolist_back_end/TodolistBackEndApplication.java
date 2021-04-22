aba39df10eff4f1fe65d6ef2c5c9eea2da4cc33c
SanoP <sanodeemantha@gmail.com>
2021-04-22 14:46:15 +0530

    Initial commit

---
+++ src/main/java/com/todolist/todolist_back_end/TodolistBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.todolist_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistBackEndApplication.class, args);
+	}
+
+}
