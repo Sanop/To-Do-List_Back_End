7bcf286062e79f5f556af1b655ae246b2eea22bb
SanoP <sanodeemantha@gmail.com>
2021-04-22 16:13:19 +0530

    create get all todos method

--- src/main/java/com/todolist/todolist_back_end/controller/ToDoController.java
+++ src/main/java/com/todolist/todolist_back_end/controller/ToDoController.java
@@ -5,6 +5,8 @@ import com.todolist.todolist_back_end.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 /**
  * @author : K.S.P.D De Silva <sanodeemantha@gmail.com>
  * @since : 4/22/21
@@ -25,5 +27,16 @@ public class ToDoController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @GetMapping
+    public List<Todos> getAllToDos(){
+        List<Todos> allToDos = new ArrayList<>();
+        try {
+            allToDos = toDoService.getAllToDos();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return allToDos;
     }
 }
